fe55977861f7138ea78cb904247b6b7966198241
Ryan Buendorf <ryansbuendorf@gmail.com>
2021-05-29 12:54:47 -0500

    can parse the file a print total player stats for a single map

--- player-stats/src/main/java/gopherwatch_stats_objects/Player.java
+++ player-stats/src/main/java/gopherwatch_stats_objects/Player.java
@@ -6,22 +6,24 @@ public class Player {
     private String name;
     private int elims;
     private int finalBlows;
-    private int damageDone;
+    private double damageDone;
     private int deaths;
-    private int healing;
-    private int blocked;
-    private int timePlayed;
+    private double healing;
+    private double blocked;
+    private double timePlayed;
     private ArrayList<Hero> heroesPlayed = new ArrayList<>();
+    private Hero currentHero;
 
     public Player(String name) {
         this.name = name;
         this.elims = 0;
         this.finalBlows = 0;
-        this.damageDone = 0;
+        this.damageDone = 0.0;
         this.deaths = 0;
-        this.healing = 0;
-        this.blocked = 0;
-        this.timePlayed = 0;
+        this.healing = 0.0;
+        this.blocked = 0.0;
+        this.timePlayed = 0.0;
+        this.currentHero = null;
     }
 
     public String getName() { return name; }
@@ -30,15 +32,17 @@ public class Player {
 
     public int getFinalBlows() { return finalBlows; }
 
-    public int getDamageDone() { return damageDone; }
+    public double getDamageDone() { return damageDone; }
 
     public int getDeaths() { return deaths; }
 
-    public int getHealing() { return healing; }
+    public double getHealing() { return healing; }
 
-    public int getBlocked() { return blocked; }
+    public double getBlocked() { return blocked; }
 
-    public int getTimePlayed() { return timePlayed; }
+    public double getTimePlayed() { return timePlayed; }
+
+    public Hero getCurrentHero()  { return currentHero; }
 
     public void setName(String name) { this.name = name; }
 
@@ -46,15 +50,17 @@ public class Player {
 
     public void setFinalBlows(int finalBlows) { this.finalBlows = finalBlows; }
 
-    public void setDamageDone(int damageDone) { this.damageDone = damageDone; }
+    public void setDamageDone(double damageDone) { this.damageDone = damageDone; }
 
     public void setDeaths(int deaths) { this.deaths = deaths; }
 
-    public void setHealing(int healing) { this.healing = healing; }
+    public void setHealing(double healing) { this.healing = healing; }
 
-    public void setBlocked(int blocked) { this.blocked = blocked; }
+    public void setBlocked(double blocked) { this.blocked = blocked; }
 
-    public void setTimePlayed(int timePlayed) { this.timePlayed = timePlayed; }
+    public void setTimePlayed(double timePlayed) { this.timePlayed = timePlayed; }
+
+    public void setCurrentHero(Hero currentHero)  { this.currentHero = currentHero; }
 
     public ArrayList<Hero> getHeroes() { return heroesPlayed; }
 }
--- player-stats/src/main/java/ui/Parser.java
+++ player-stats/src/main/java/ui/Parser.java
@@ -1,5 +1,6 @@
 package ui;
 
+import gopherwatch_stats_objects.Hero;
 import gopherwatch_stats_objects.Player;
 
 import java.io.FileInputStream;
@@ -12,22 +13,36 @@ public class Parser {
     private ArrayList<Player> playersInMap = new ArrayList<>();
 
     public void Run() throws FileNotFoundException {
-        System.out.println("App is running");
-        Scanner console = new Scanner(System.in);
+        // Scanner console = new Scanner(System.in);
 
         // TODO CREATE THE ABILITY TO ENTER ANOTHER FILE NAME
         FileInputStream fIn = new FileInputStream("C:\\Users\\Ryan\\Documents\\Overwatch\\Workshop\\Log-2021-05-03-22-59-19.txt");
         Scanner fileReader = new Scanner(fIn);
 
-        String mapName = fileReader.nextLine();
-        System.out.println(mapName);
+        String mapName = fileReader.nextLine().substring(11);
 
         // creates the players
         playersInMap = generatePlayers(fileReader);
 
         // TODO PARSE REST OF FILE FOR PLAYER STATS
 
+        while(fileReader.hasNext()){
+            updatePlayers(playersInMap, fileReader, mapName);
+        }
+
         fileReader.close();
+
+        for(Player player: playersInMap) {
+            System.out.printf("%nPlayer Name: %s%n", player.getName());
+            System.out.println("=".repeat(player.getName().length() + 13));
+            System.out.printf("Time Played: %.2f%n", player.getTimePlayed());
+            System.out.printf("Eliminations: %d%n", player.getElims());
+            System.out.printf("Final Blows: %d%n", player.getFinalBlows());
+            System.out.printf("Damage Done: %.2f%n", player.getDamageDone());
+            System.out.printf("Deaths: %d%n", player.getDeaths());
+            System.out.printf("Healing Done: %.2f%n", player.getHealing());
+            System.out.printf("Damage Blocked: %.2f%n", player.getBlocked());
+        }
     }
 
     public static String readString(String prompt, Scanner console) {
@@ -45,5 +60,33 @@ public class Parser {
         }
 
         return players;
+    }
+
+    public static void updatePlayers(ArrayList<Player> players, Scanner fileReader, String mapName) {
+        for(Player player: players) {
+            String line = fileReader.nextLine().substring(11);
+            System.out.println(line);
+            if(line.equals(mapName)) {
+                line = fileReader.nextLine();
+                line = fileReader.nextLine().substring(11);
+            }
+            String[] tokens = line.split(",");
+
+            // Update player total stats
+            player.setTimePlayed(Double.parseDouble(tokens[0]));
+            player.setElims(Integer.parseInt(tokens[3]));
+            player.setFinalBlows(Integer.parseInt(tokens[4]));
+            player.setDamageDone(Double.parseDouble(tokens[5]));
+            player.setDeaths(Integer.parseInt(tokens[6]));
+            player.setHealing(Double.parseDouble(tokens[7]));
+            player.setBlocked(Double.parseDouble(tokens[8]));
+
+            /*
+            Hero current = player.getCurrentHero();
+            if(!current.getName().equals(tokens[2])) {
+
+            }
+             */
+        }
     }
 }
